7c091c165960abfe1a8706f79a3ab4f31295f5e4
rybo1217 <brownryan1217@yahoo.com>
2022-08-15 18:59:36 -0400

    gotta fiox math for plates

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+function storeVar(el) {
+    bar = parseInt(el.value); //barbell weight
+    ff=document.getElementById("ff").value;
+    tf=document.getElementById("tf").value;
+    tff=document.getElementById("tff").value;
+    te=document.getElementById("te").value;
+    fi=document.getElementById("fi").value;
+    tp=document.getElementById("tp").value;//fix math for the plates
+   console.log(bar+bar);//tester
+
+
+  } 
+
+
+
