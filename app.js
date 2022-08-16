640bc53453aa3cfa9dc8de7852e30c8e909f5b70
rybo1217 <brownryan1217@yahoo.com>
2022-08-16 00:00:31 -0400

    weight calc

--- app.js
+++ app.js
@@ -5,13 +5,16 @@
 
 function storeVar(el) {
     bar = parseInt(el.value); //barbell weight
-    ff=document.getElementById("ff").value;
-    tf=document.getElementById("tf").value;
-    tff=document.getElementById("tff").value;
-    te=document.getElementById("te").value;
-    fi=document.getElementById("fi").value;
-    tp=document.getElementById("tp").value;//fix math for the plates
-   console.log(bar+bar);//tester
+    ff=parseInt(document.getElementById("ff").value);
+    tf=parseInt(document.getElementById("tf").value);
+    tff=parseInt(document.getElementById("tff").value);
+    te=parseInt(document.getElementById("te").value);
+    fi=parseInt(document.getElementById("fi").value);
+    tp=parseInt(document.getElementById("tp").value);//fix math for the plates
+    plates=parseInt((ff*45)+(tf*35)+(tff*25)+(te*10)+(fi*5)+(tp*2.5));
+    totWeight=plates+bar;
+    document.getElementById('totalWeight').innerHTML = `There is ${totWeight}lbs on the bar.`;
+   console.log(bar+plates);//tester
 
 
   } 
